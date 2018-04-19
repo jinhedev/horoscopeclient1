f16647c18f4ebdb83648dd6efb55ec729a4b3a20
jinhedev <jinhedev@gmail.com>
2018-04-19 11:00:07 -0700

    Updates WebServicesManager and WebServiceType for objc compatibility

--- Lib/WebServicesManager.swift
+++ Lib/WebServicesManager.swift
@@ -10,30 +10,23 @@ import Foundation
 import Alamofire
 import UIKit
 
-protocol WebServiceManagerDelegate: NSObjectProtocol {
+@objc public protocol WebServiceManagerDelegate: NSObjectProtocol {
     func webService(_ manager: WebServiceManager, didErr error: Error, type: WebServiceType)
-    func webService(_ manager: WebServiceManager, didFetch data: Any, type: WebServiceType)
-    func webService(_ manager: WebServiceManager, didPost data: Any, type: WebServiceType)
-    func webService(_ manager: WebServiceManager, didPatch data: Any, type: WebServiceType)
-    func webService(_ manager: WebServiceManager, didDelete data: Any, type: WebServiceType)
-}
-
-extension WebServiceManagerDelegate {
-    func webService(_ manager: WebServiceManager, didFetch data: Any, type: WebServiceType) {}
-    func webService(_ manager: WebServiceManager, didPost data: Any, type: WebServiceType) {}
-    func webService(_ manager: WebServiceManager, didPatch data: Any, type: WebServiceType) {}
-    func webService(_ manager: WebServiceManager, didDelete data: Any, type: WebServiceType) {}
+    @objc optional func webService(_ manager: WebServiceManager, didFetch data: Any, type: WebServiceType)
+    @objc optional func webService(_ manager: WebServiceManager, didPost data: Any, type: WebServiceType)
+    @objc optional func webService(_ manager: WebServiceManager, didPatch data: Any, type: WebServiceType)
+    @objc optional func webService(_ manager: WebServiceManager, didDelete data: Any, type: WebServiceType)
 }
 
 /**
  WebServiceType is a user-define enum that specifies what type of data response the user expect to get.
  - warning: Expected response may or may not be the actual response. Be warned!
  */
-enum WebServiceType {
+@objc public enum WebServiceType: Int {
     case tests
 }
 
-class WebServiceManager: NSObject {
+public class WebServiceManager: NSObject {
 
     weak var delegate: WebServiceManagerDelegate?
 
@@ -41,7 +34,7 @@ class WebServiceManager: NSObject {
         Alamofire.request(fromUrl, method: HTTPMethod.get, parameters: parameters, encoding: URLEncoding.default, headers: headers).responseJSON(queue: DispatchQueue.main, options: JSONSerialization.ReadingOptions.mutableContainers) { (response) in
             switch response.result {
             case .success(let value):
-                self.delegate?.webService(self, didFetch: value, type: type)
+                self.delegate?.webService?(self, didFetch: value, type: type)
             case .failure(let error):
                 self.delegate?.webService(self, didErr: error, type: type)
             }
@@ -52,7 +45,7 @@ class WebServiceManager: NSObject {
         Alamofire.request(fromUrl, method: HTTPMethod.post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseJSON(queue: DispatchQueue.main, options: JSONSerialization.ReadingOptions.mutableContainers) { (response) in
             switch response.result {
             case .success(let value):
-                self.delegate?.webService(self, didPost: value, type: type)
+                self.delegate?.webService?(self, didPost: value, type: type)
             case .failure(let error):
                 self.delegate?.webService(self, didErr: error, type: type)
             }
